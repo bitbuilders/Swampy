a0c8fc11cb66c158972c33fc09b1818f8515a6cc
Unknown <WEller@student.neumont.edu>
2019-03-05 11:29:49 -0700

    Added More Database functions
    Added UpdateAvatar, Login, MakeNewMessage, and fixed pushToDB(register function)

--- routes/database.js
+++ routes/database.js
@@ -19,21 +19,103 @@ var userSchema = mongoose.Schema({
     age: Number
 });
 
-var User = mongoose.model("User_Collection", userSchema);
+var messageSchema = mongoose.Schema({
+    username: String,
+    title: String,
+    message: String
+});
 
-exports.pushToDB = req => {
-    var user = new User({
+var User = mongoose.model("user_collection", userSchema);
+var Message = mongoose.model("message_thread", messageSchema);
+
+exports.pushToDB = (req, res) => {
+    let user = new User();
+    User.findOne({username: req.body.username}, function(err, results){
+        if(!results){
+            //this found nothing make a new user
+            user = new User({
+                username: req.body.username,
+                password: req.body.password,
+                // avatar: api call using the req.body.username,
+                isAdmin: req.body.admin,
+                email: req.body.email,
+                age: req.body.age
+            });
+            User.create(user);
+            console.log("User should've created");
+            var ugh = {
+                user,
+                error: ""
+            }
+            return ugh;
+        }
+        else{
+            //this user already exists
+            //return a user and the error message if he already exists
+            var ugh = {
+                user: "",
+                error: "User already exists"
+            }
+            return ugh;
+        }
+    });
+}
+
+exports.MakeNewMessage = (req, res) => {
+    let mess = new Message({
         username: req.body.username,
-        password: req.body.password,
-        // avatar: api call using the req.body.username,
-        isAdmin: req.body.admin,
-        email: req.body.email,
-        age: req.body.age
+        title: req.body.title,
+        message: req.body.message
     });
-    user.save(function(err, user){
-        if(err)return err;
-        console.log(req.body.username + "was added");
-    });
+    Message.create(mess);
+    console.log("Message should've created");
+}
+
+exports.UpdateUserAvatar = (username, avatar) => {
+    User.findOne({username: username}, function(err,results){
+        if(!results){
+            //this found nothing and should be impossiple 
+        }else{
+            //this found something
+            // bleh = new User({
+            //     avatar: avatar
+            // });
+            results.avatar = avatar;
+            results.save();
+        }
+    })
+}
+
+exports.Login = (username, password) => {
+    let bleh = new User();
+    User.findOne({username: username}, function(err, results){
+        if(!results){
+            //found nothing so failed login
+            bleh = {
+                error: "Login Failed Username not found"
+            }
+        }else{
+            //found something so log them in
+            User.findOne({password: password}, function(err, otherResults){
+                //i still need to check of the password hash is actually the true password
+                if(!otherResults){
+                    //found nothing so login still failed
+                    bleh = {
+                        error: "Login Failed becase of password"
+                    }
+                }
+                bleh = new User({
+                    username: results.username,
+                    password: results.password,
+                    avatar: results.avatar,
+                    isAdmin: results.isAdmin,
+                    email: results.email,
+                    age: results.age
+                });
+            })
+        }
+        return bleh
+    })
 }
 
 // exports.create = () => {
