89e257d5494c584191f4fdf3edd34a5b724f997d
Unknown <WEller@student.neumont.edu>
2019-03-08 11:53:07 -0700

    Added some methods
    like message delete and edit and some others

--- routes/database.js
+++ routes/database.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var hash;
 // mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Data', {
     useNewUrlParser:true
@@ -21,8 +23,9 @@ var userSchema = mongoose.Schema({
 
 var messageSchema = mongoose.Schema({
     username: String,
-    title: String,
-    message: String
+    date: String,
+    message: String,
+    imageURL: String
 });
 
 var User = mongoose.model("user_collection", userSchema);
@@ -34,11 +37,15 @@ async function pushToDB(user) {
     var userResult = await User.findOne({username: user.username}).exec();
 
     console.log(userResult);
-
+    var hashedPassword;
     if(!userResult){
+        bcrypt.hash(user.password,null,null,function(err,hash){
+            console.log("idk what is suppposed to happen here: " + hash)
+            var hashedPassword = hash;
+        });
         bleh = new User({
             username: user.username,
-            password: user.password,
+            password: hashedPassword,
             imageUrl: user.imageUrl,
             isAdmin: user.isAdmin,
             email: user.email,
@@ -54,56 +61,58 @@ async function pushToDB(user) {
         return ugh;
     } else {
         //this user already exists
-        //return a user and the error message if he already exists
         var ugh = {
             user: null,
             error: "User already exists"
         }
         return ugh;
     }
-
-    // User.findOne({username: user.username}, async function(err, results){
-    //     console.log(results);
-    //     if(!results){
-    //         //this found nothing make a new user
-    //         bleh = new User({
-    //             username: user.username,
-    //             password: user.password,
-    //             imageUrl: user.imageUrl,
-    //             isAdmin: user.isAdmin,
-    //             email: user.email,
-    //             age: user.age
-    //         });
-    //         var result = await User.create(bleh);
-    //         console.log("User should've created");
-    //         var ugh = {
-    //             user: bleh,
-    //             error: ""
-    //         }
-    //         // return ugh;
-    //         return result;
-    //     }
-    //     else{
-    //         //this user already exists
-    //         //return a user and the error message if he already exists
-    //         var ugh = {
-    //             user: null,
-    //             error: "User already exists"
-    //         }
-    //         return ugh;
-    //     }
-    // });
 }
+//this is the export for the register function
 exports.pushToDB = pushToDB;
 
+//this will take in a message 
 exports.makeNewMessage = (req, res) => {
     let mess = new Message({
         username: req.body.username,
-        title: req.body.title,
+        date: req.body.title,
         message: req.body.message
     });
     Message.create(mess);
     console.log("Message should've created");
+}
+
+//hopefully just edits the message that was passed in
+exports.editMessage = (message) => {
+    Message.findOneAndUpdate({_id: message._id}, {message: message.message, date: message.date})
+}
+
+//should just blow up and delete the message
+exports.deleteMessage = (message) => {
+    Message.findByIdAndDelete({_id: message._id})
+}
+
+//should return an array of all messages
+exports.getAllMessages = () =>{
+    bleh = [];
+    Message.find()
+    .exec(function(err, allMessages){
+        if(err) return console.log(err);
+        for (let i = 0; i < allMessages.length; i++) {
+            const element = allMessages[i];
+            console.log("This is the entire message: " + element)
+            bleh.push(
+                {
+                    messageinfo: allMessages[i], 
+                    imageURL: User.findOne({username: allMessages[i]}).imageURL
+                }
+            )
+        }
+        return bleh;
+    })
+    //do a database call to get the avatar image
+    //store that and all messages into a custom object
+    //return those custom objects
 }
 
 exports.updateUserAvatar = (username, imageUrl) => {
@@ -129,11 +138,23 @@ exports.login = async (username, password) => {
         return {error: "Login Failed Username not found"};
     }
 
-    if(user.password == password){
-        return  {user: user};
-    } else {
-        return {error: "Login Failed becase of password"};
-    }
+    // if(user.password == password){
+    //     return  {user: user};
+    // } else {
+    //     return {error: "Login Failed becase of password"};
+    // }
+    bcrypt.compare(user.password, password, function(err, res){
+        if(res){
+            //the password matchs what was passed in
+            return {user: user};
+        }else{
+            //the password does not match the what was passed in
+            return {error: "Login failed because of password"}
+        }
+    });
+
+
+
     // , function(err, results){
     //     if(!results){
     //         //found nothing so failed login
