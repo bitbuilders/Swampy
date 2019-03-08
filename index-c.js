8cc5b96d7ca157ec20d555ec71490d5d21153f03
Colin Misbach <colin@misbach.com>
2019-03-08 11:39:43 -0700

    Added more styling to home page

--- index-c.js
+++ index-c.js
@@ -14,7 +14,6 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, '/public')))
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
